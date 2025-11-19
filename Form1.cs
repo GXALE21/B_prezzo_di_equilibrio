5ae24fdc42f9a812060efae1f240e5d0ae24b687
BUMMA.21102 <BUMMA.21102@LABI1-13PC17>
2025-11-19 13:49:06 +0100

    public class ParametriMercato

--- Form1.cs
+++ Form1.cs
@@ -8,13 +8,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace ese_prezzo_di_equilibrio
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+        public class ParametriMercato
+        {
+            public double DomandaIntercetta { get; set; } = 90;
+            public double DomandaCoefficiente { get; set; } = 4;
+            public double OffertaCostante { get; set; } = 10;
+            public double OffertaCoefficiente { get; set; } = 0.01;
+            public double OffertaEsponente { get; set; } = 3;
         }
 
         private void Form1_Load(object sender, EventArgs e)
